1aab8f6bf114cdbc358c03891408f6c85ffaffaa
Stephen Jayakar <aj12ay@gmail.com>
2026-03-27 18:33:12 -0700

    oops

--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,11 @@
 const config = {
   testDir: './tests',
   timeout: 30000,
+  webServer: {
+    command: 'npm run dev -- --host 127.0.0.1 --port 3000',
+    port: 3000,
+    reuseExistingServer: true,
+  },
   use: {
     baseURL: process.env.PLAYWRIGHT_BASE_URL || 'http://127.0.0.1:3000',
     viewport: { width: 1440, height: 1200 },
