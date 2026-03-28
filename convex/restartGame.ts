c4157f8e58e53690e644ed1babbe743b83634976
Stephen Jayakar <aj12ay@gmail.com>
2026-03-27 18:52:51 -0700

    Make run it back restart immediately

--- convex/restartGame.ts
+++ convex/restartGame.ts
@@ -1,16 +1,19 @@
 import { mutation } from './_generated/server'
-import { GAME_TABLE } from './common'
+import { GAME_TABLE, createLevers } from './common'
 
 export const restartGame = mutation({
   args: {},
   handler: async ({ db }) => {
     const gameState = await db.query(GAME_TABLE).first()
     if (gameState !== null) {
+      const players = gameState.players.map(() => ({ alive: true }))
+      const canRestartImmediately = players.length > 1
+
       await db.patch(gameState._id, {
-        players: gameState.players.map(() => ({ alive: true })),
-        levers: [],
-        isStarted: false,
-        playerTurn: undefined,
+        players,
+        levers: canRestartImmediately ? createLevers(players.length + 1) : [],
+        isStarted: canRestartImmediately,
+        playerTurn: canRestartImmediately ? 0 : undefined,
       })
     }
   },
